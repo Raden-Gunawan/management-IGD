ea37cd1e91222f922a463a50ba79a4bd8eafa8de
Bintang1213 <140826237+Bintang1213@users.noreply.github.com>
2026-01-19 15:52:13 +0700

    toto

--- Backend/main.go
+++ Backend/main.go
@@ -5,6 +5,7 @@ import (
 	"backend/models"
 	"backend/routes"
 
+	"github.com/gin-contrib/cors" 
 	"github.com/gin-gonic/gin"
 )
 
@@ -14,6 +15,16 @@ func main() {
 	config.DB.AutoMigrate(&models.User{})
 
 	r := gin.Default()
+
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:5173"}, 
+		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+	}))
+
+
 	routes.RegisterRoutes(r)
 
 	r.Run(":8080")
